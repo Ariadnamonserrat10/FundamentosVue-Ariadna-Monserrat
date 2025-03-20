211be6f499944ac875c693cd2aa36d7528c0bdee
Ariadnamonserrat10 <aparicioariadna224@gmail.com>
2025-03-20 14:49:32 -0600

    Subiendo carpeta inicial

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+//metodos
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+ <!--estructura html-->
+ <HelloWorld msg = "Vite + Vue" />
+</template>
+
+<style scoped>
+/*Estilos css*/
+
+</style>
