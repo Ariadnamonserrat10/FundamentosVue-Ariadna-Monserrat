da4a55bf9a890ad80352929f555f1bc41d73af87
Ariadnamonserrat10 <aparicioariadna224@gmail.com>
2025-03-23 23:49:38 -0600

    integracion del cardprincipal

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,7 @@
 <script setup>
-//importacion del card principal
-import CardPrincipal from './CardPrincipal.vue';
 //importacion del primer card
 import Card from './Card.vue';
+//importacion del card principal
 import CardPrincipal from './CardPrincipal.vue';
 //metodos
 </script>
